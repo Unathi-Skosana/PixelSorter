5cabff5fb8d1e303c4ad4cd26ea98dccc4265c7b
Bhekimpilo <18998712@sun.ac.za>
2018-05-23 18:13:12 +0200

    modified with help of Unathi

--- src/ImageFeatureVector.py
+++ src/ImageFeatureVector.py
@@ -4,36 +4,66 @@ from PIL import Image
 
 class ImageFeatureVector(object):
     ''' docstring '''
-    def __init__(self, img_name, sort_criteria):
+    def __init__(self, img_name, dest_img_path, sort_criteria):
         self.img_name             = img_name
+        self.dest_img_path        = dest_img_path
         self.sort_criteria        = sort_criteria
         self.pixel_data           = None
         self.criteria_data        = None
+        self.img                  = None
         self.__process_img__()
 
     def __process_img__(self):
-        ''' docstring '''
-        img = Image.open(self.img_name)
+        '''
+        This is a helper method that is used to read in the data of the source image. This method
+        gets all the pixel data of the source image to be edited.
+        '''
+        self.img = Image.open(self.img_name)
         lum = lambda r, g, b: r*0.3 + g*0.59 + b*0.11        # luminance
         chr = lambda r, g, b: max(r, g, b) - min(r, g, b)    # chroma
-        self.pixel_data = list(img.getdata())
+        self.pixel_data = self.img.getdata()
         pixels_criteria = []
         if self.sort_criteria == 'L':
-            for rgb in self.pixel_data:
-                pixels_criteria.append(lum(rgb[0], rgb[1], rgb[2]))
+            for (r, g, b) in self.pixel_data:
+                pixels_criteria.append(lum(r, g, b))
         elif self.sort_criteria == 'C':
-            for rgb in self.pixel_data:
-                pixels_criteria.append(chr(rgb[0], rgb[1], rgb[2]))
+            for r, g, b in self.pixel_data:
+                pixels_criteria.append(chr(r, g, b))
         self.criteria_data = pixels_criteria
+
+    def create_sorted_image(self, criteria_data, pixel_data):
+        '''
+        this method is responseble for writing the final edited or sorted image to the
+        destination path
+        '''
+        temp = self.img.copy()
+        #print tuple()
+        #temp.putdata()
+        #temp.save()
+        #print 'coppied'
+        #print(help(Image))
+
 
     def get_criteria_data(self):
         ''' docstring '''
         return self.criteria_data
 
     def get_pixel_data(self):
-        ''' docstring '''
+        '''
+        get the data for individual pixels, the return type of this method is a length
+        3 turple that has individual pixel RGB data. NOTE: these are actually the original
+        pixel data of the image that was read from the source image to be edited
+        '''
         return self.pixel_data
 
+    def get_img_destination_path(self):
+        '''
+        returns the destination or path of the resulting image, that has been sorted.
+        '''
+        return self.dest_img_path
+
     def get_image_name(self):
-        ''' docstring '''
+        '''
+        returns the original image name that was being sorted or that was being edited.
+        '''
         return self.img_name
--- src/PixelSorter.py
+++ src/PixelSorter.py
@@ -6,8 +6,9 @@ from sys import argv, exit
 
 class PixelSorter(object):
     ''' docstring '''
-    def __init__(self, image_name, sort_criteria):
-        self.image_name = image_name
+    def __init__(self, src_img, dest_img, sort_criteria):
+        self.src_img = src_img
+        self.dest_img = dest_img
         self.sort_criteria = sort_criteria
         self.__validate_args__()
 
@@ -16,17 +17,17 @@ class PixelSorter(object):
         if not (self.sort_criteria == 'C' or self.sort_criteria == 'L'):
             raise IOError
 
-    def sort_pixels(self):
+    def pixel_sorter_middleware(self):
         ''' docstring '''
-        ifv           = IFV(self.image_name, self.sort_criteria)
+        ifv           = IFV(self.src_img, self.dest_img, self.sort_criteria)
         pixel_data    = ifv.get_pixel_data()
         criteria_data = ifv.get_criteria_data()
         comparative_merge_sort(criteria_data, pixel_data)
+        ifv.create_sorted_image(criteria_data, pixel_data)
 
 if __name__ == '__main__':
-    print len(argv)
-    if len(argv) == 3:
-        window = PixelSorter(argv[1], argv[2])
-        window.sort_pixels()
+    if len(argv) == 4:
+        window = PixelSorter(argv[1], argv[2], argv[3])
+        window.pixel_sorter_middleware()
     else:
-        exit('USAGE: python PixelSorter.py <image-path> <sort-criteria>')
+        exit('USAGE: python PixelSorter.py <source-img-path> <dest-img-path> <sort-criteria>')
